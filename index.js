70b51498073328025e7118ebcca1da9f1f023f1d
caiquefigueredo <caiqueleandro1@gmail.com>
2022-06-12 19:30:12 -0300

    1 file modified

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-// run `node index.js` in the terminal
-
-console.log(`Hello Node.js v${process.versions.node}!`);
+const somosCasados = true //varíavel boolean 0 ou 1, true ou false
+const anoNascimento = 1997 //varíavel number
+const media = (7+8+9+10) /4 //float number
+const nome = "Caique" //variável string
+let idade = null //variável null, idade vai oferecer alterações por isso let
+const filho // undefined declaração sem atribuição
+const numeroGrande = 21214214214142n 
+const numeroGrande2 = BigInt("23589538532985025n")
+//variável BigInt duas formas usado para numeros muito numeroGrande
+const meuSimbolo = new Symbol() //variavel Symbol
+const meuObjeto = {
+  curso: "Estrutura de Dados",
+  instrutores: ['Gabriel', 'Vanessa']
+} //object padrão JSON (Javascript objetic notation)
